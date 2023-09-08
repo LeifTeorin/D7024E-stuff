020e3fff56cdd1624ba50333abfe77c28b099f00
Hjalmar <hjaolo-9@student.ltu.se>
2023-09-08 17:08:15 +0200

    a bare bones ping and message function

--- Go/go.mod
+++ Go/go.mod
@@ -1,3 +1,4 @@
 module github.com/LeifTeorin/Go
 
-go 1.18
+go 1.21
+
--- Go/kademlia/network.go
+++ Go/kademlia/network.go
@@ -3,10 +3,17 @@ package kademlia
 import (
 	"fmt"
 	"net"
+	"time"
+	"encoding/json"
 )
 
 type Network struct {
 	routingTable *RoutingTable
+}
+
+type Message struct {
+	messageType string
+	content string
 }
 
 func Listen(ip string, port int) error {
@@ -28,8 +35,51 @@ func Listen(ip string, port int) error {
 	}
 }
 
-func (network *Network) SendPingMessage(contact *Contact) {
-	// TODO
+func (network *Network) SendMessage (msg Message, address string) ([]byte, error){
+	conn, err := net.Dial("tcp", address)
+	if err != nil {
+		return nil, err
+	}
+	data, _ := json.Marshal(msg)
+	_, err = conn.Write(data)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// Set a timeout for read and write operations (adjust as needed)
+    deadline := time.Now().Add(5 * time.Second)
+    conn.SetDeadline(deadline)
+
+	// Read and process the response 
+    response := make([]byte, 1024) // Adjust buffer size as needed
+    n, err := conn.Read(response)
+    if err != nil {
+        return nil, err
+    }
+
+    // Return the response data (trim excess buffer if needed)
+    return response[:n], nil
+}
+
+func (network *Network) SendPingMessage(contact *Contact) bool{
+	ping := Message {
+		"PING",
+		network.routingTable.me.Address,		// maybe we should change this cause it's kinda annoying
+	}
+	_, err := network.SendMessage(ping, contact.Address)
+	if err != nil {
+		fmt.Sprintf("ping failed :(")
+		return false
+	}
+	// var message Message
+	// unmarschalerr := json.Unmarschal(response, &message)
+
+	// if unmarschalerr != nil {
+	// 	fmt.Sprintf("ping failed :(")
+	// 	return false
+	// }
+	return true // just to see if it went right for now
 }
 
 func (network *Network) SendFindContactMessage(contact *Contact) {
